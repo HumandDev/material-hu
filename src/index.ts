cd15a4a75ae281044e733b8ddbefa723106102c2
Mariano Zicavo <marianozicavo@gmail.com>
2023-06-30 11:21:07 -0300

    Add scripts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-const a = 'qwe';
-
-export default a;
+export * as iconsmaterial from '@mui/icons-material';
+export * as lab from '@mui/lab';
+export * as material from '@mui/material';
+export * as datepickers from '@mui/x-date-pickers';
