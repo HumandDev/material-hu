cd8c1e1f6483210a0a4569884965d4b71339d0cc
Mariano Zicavo <marianozicavo@gmail.com>
2023-06-30 17:21:36 -0300

    w

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from '@mui/material';
-export { LoadingButton as Button } from '@mui/lab';
-export type { LoadingButtonProps as ButtonProps } from '@mui/lab';
+export {default as Button, LoadingButtonProps as ButtonProps } from '@mui/lab/LoadingButton';
 export * from '@mui/x-date-pickers';
 export * from '@mui/x-date-pickers/AdapterDateFns';
