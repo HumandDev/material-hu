2e387cabc839bb3ca3eebf7c9cbeeac49b7c6ec2
Mariano Zicavo <marianozicavo@gmail.com>
2023-06-30 11:36:32 -0300

    q

---
+++ src/iconsmaterial/index.ts
@@ -0,0 +1,1 @@
+export * as iconsmaterial from '@mui/icons-material';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export * as iconsmaterial from '@mui/icons-material';
 export * as lab from '@mui/lab';
 export * as material from '@mui/material';
 export * as datepickers from '@mui/x-date-pickers';
