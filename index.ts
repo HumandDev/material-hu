f1462356e8f5e4cd6e841ed4a4e9ef1ad8c3c3d3
Mariano Zicavo <marianozicavo@gmail.com>
2023-06-30 14:08:19 -0300

    w

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@mui/material';
-export * as lab from '@mui/lab';
-export * from '@mui/x-date-pickers';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-
+export * from '@mui/material';
+export * as lab from '@mui/lab';
+export * from '@mui/x-date-pickers';
