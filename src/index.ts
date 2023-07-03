4668373c1f1ec15ba3ae3014c0d7d37f4a838075
Julián Vuoso <juvuoso@itba.edu.ar>
2023-07-03 16:55:41 -0300

    -

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from '@mui/material';
 export * from '@mui/x-date-pickers';
 export * from '@mui/x-date-pickers/AdapterDateFns';
-export { default as Button } from '@mui/lab/LoadingButton';
-export { type LoadingButtonProps as ButtonProps } from '@mui/lab/LoadingButton';
+export { default as Button, type LoadingButtonProps as ButtonProps } from '@mui/lab/LoadingButton';
