0627c3cd2024c7f48ccae1c58b181e8b24315d21
Julián Vuoso <juvuoso@itba.edu.ar>
2023-07-03 14:55:38 -0300

    dont rename

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from '@mui/material';
 export * from '@mui/x-date-pickers';
 export * from '@mui/x-date-pickers/AdapterDateFns';
-export { default as Button} from '@mui/lab/LoadingButton';
-export { LoadingButtonProps as ButtonProps } from '@mui/lab/LoadingButton';
+export { default as Button, LoadingButtonProps } from '@mui/lab/LoadingButton';
