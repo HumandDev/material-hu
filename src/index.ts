91f21d131cd691dc0eae274365a4f2e781037c6f
Mariano Zicavo <marianozicavo@gmail.com>
2023-07-04 11:17:53 -0300

    w

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from '@mui/material';
 export * from '@mui/x-date-pickers';
 export * from '@mui/x-date-pickers/AdapterDateFns';
+export * from '@mui/x-date-pickers/internals/hooks/useDesktopPicker'
 export { default as Button } from '@mui/lab/LoadingButton';
