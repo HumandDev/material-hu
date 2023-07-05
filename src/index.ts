94c78a322754bdc652379b171998a8b778547407
Julián Vuoso <juvuoso@itba.edu.ar>
2023-07-05 16:57:34 -0300

    add theme

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from '@mui/x-date-pickers';
 export * from '@mui/x-date-pickers/AdapterDateFns';
 export * from '@mui/x-date-pickers/internals/hooks/useDesktopPicker'
 export { default as Button } from '@mui/lab/LoadingButton';
+export { createTheme as createNewTheme } from '../theme/material-hu';
---
+++ theme/base/create-options.ts
@@ -0,0 +1,56 @@
+import { ThemeOptions, colors } from '../../src';
+import type { Direction } from '../material-hu';
+import { createTypography } from './create-typography';
+import { createComponents } from './create-components';
+
+interface Config {
+  direction?: Direction;
+  color?: string
+}
+
+// Here we do not modify the "palette" and "shadows" because "light" and "dark" mode
+// may have different values.
+
+export const createOptions = (config: Config): ThemeOptions => {
+  const { direction = 'ltr' } = config;
+
+  return {
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1440
+      }
+    },
+    components: createComponents(),
+    direction,
+    shape: {
+      borderRadius: 8
+    },
+    typography: createTypography(),
+    palette: {
+      primary: {
+        contrastText: '#ffffff',
+        main: config?.color || 'blue',
+        // lighter: '#DFEDFF'
+      },
+      secondary: {
+        main: colors.grey[600]// '#757575',
+      },
+      success: {
+        contrastText: '#ffffff',
+        main: '#4caf50'
+      },
+      text: {
+        primary: '#172b4d',
+        secondary: '#6b778c'
+      },
+      warning: {
+        contrastText: '#ffffff',
+        main: '#ff9800'
+      }
+    },
+  };
+};
