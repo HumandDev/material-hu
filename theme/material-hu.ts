ff393205564b47f5a24cbefd4251905b779b123f
Julián Vuoso <juvuoso@itba.edu.ar>
2023-07-06 10:11:52 -0300

    -

--- theme/material-hu.ts
+++ theme/material-hu.ts
@@ -1,42 +1,40 @@
 import { Theme, createTheme as createMuiTheme, responsiveFontSizes } from '../src';
 import { createOptions } from './base/create-options';
 
-declare module '@mui/material' {
-  interface ColorRange {
-    50: string;
-    100: string;
-    200: string;
-    300: string;
-    400: string;
-    500: string;
-    600: string;
-    700: string;
-    800: string;
-    900: string;
-  }
+interface ColorRange {
+  50: string;
+  100: string;
+  200: string;
+  300: string;
+  400: string;
+  500: string;
+  600: string;
+  700: string;
+  800: string;
+  900: string;
+}
 
-  interface Palette {
-    neutral: ColorRange;
-  }
+interface Palette {
+  neutral: ColorRange;
+}
 
-  interface PaletteColor {
-    lightest?: string;
-    darkest?: string;
-    alpha4?: string;
-    alpha8?: string;
-    alpha12?: string;
-    alpha30?: string;
-    alpha50?: string;
-  }
+interface PaletteColor {
+  lightest?: string;
+  darkest?: string;
+  alpha4?: string;
+  alpha8?: string;
+  alpha12?: string;
+  alpha30?: string;
+  alpha50?: string;
+}
 
-  interface PaletteOptions {
-    neutral?: ColorRange;
-  }
+interface PaletteOptions {
+  neutral?: ColorRange;
+}
 
-  interface TypeBackground {
-    paper: string;
-    default: string;
-  }
+interface TypeBackground {
+  paper: string;
+  default: string;
 }
 
 export type ColorPreset = 'blue' | 'green' | 'indigo' | 'purple';
