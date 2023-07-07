56640ac1df0454f9576d42063f1367788bdaa6b2
Mariano Zicavo <marianozicavo@gmail.com>
2023-07-07 11:58:34 -0300

    w

--- src/theme/material-hu.ts
+++ src/theme/material-hu.ts
@@ -1,43 +1,7 @@
+/// <reference path="./mui.d.ts" />
+
 import { Theme, createTheme as createMuiTheme, responsiveFontSizes } from '@mui/material';
 import createOptions from './base/create-options';
-
-declare module '@mui/material/styles' {
-  interface ColorRange {
-    50: string;
-    100: string;
-    200: string;
-    300: string;
-    400: string;
-    500: string;
-    600: string;
-    700: string;
-    800: string;
-    900: string;
-  }
-
-  interface Palette {
-    neutral: ColorRange;
-  }
-
-  interface PaletteColor {
-    lightest?: string;
-    darkest?: string;
-    alpha4?: string;
-    alpha8?: string;
-    alpha12?: string;
-    alpha30?: string;
-    alpha50?: string;
-  }
-
-  interface PaletteOptions {
-    neutral?: ColorRange;
-  }
-
-  interface TypeBackground {
-    paper: string;
-    default: string;
-  }
-}
 
 export type ColorPreset = 'blue' | 'green' | 'indigo' | 'purple';
 
