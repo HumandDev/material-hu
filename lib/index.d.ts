2d16a67b3fbe3a00cb33faad9d57249f8ac88bc0
Mariano Zicavo <marianozicavo@gmail.com>
2023-07-13 15:17:19 -0300

    move export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,5 @@ export * from '@mui/material';
 export * from '@mui/x-date-pickers';
 export * from '@mui/x-date-pickers/AdapterDateFns';
 export * from '@mui/x-date-pickers/internals/hooks/useDesktopPicker';
-export { default as Button } from '@mui/lab/LoadingButton';
+export { default as Button, type LoadingButtonProps as ButtonProps } from '@mui/lab/LoadingButton';
 export { createNewTheme } from './theme/material-hu';
-export { type LoadingButtonProps as ButtonProps } from '@mui/lab/LoadingButton';
