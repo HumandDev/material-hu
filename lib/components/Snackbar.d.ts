16fceabe489eda08057d2f810b59765988651c71
Julián Vuoso <juvuoso@itba.edu.ar>
2023-08-01 09:38:32 -0300

    new component + notistack installed

---
+++ lib/components/Snackbar.d.ts
@@ -0,0 +1,3 @@
+export declare const useSnackbarUtils: () => {
+    showSnackbar: (message: string) => void;
+};
---
+++ src/components/Snackbar.tsx
@@ -0,0 +1,21 @@
+import { useSnackbar } from 'notistack';
+import { IconButton } from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
+
+export const useSnackbarUtils = () => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  const showSnackbar = (message: string) => {
+    enqueueSnackbar(message, {
+      variant: 'default',
+      anchorOrigin: { horizontal: 'center', vertical: 'bottom' },
+      action: (
+        <IconButton onClick={() => closeSnackbar()}>
+          <CloseIcon sx={{ color: '#FFFFFF', fontSize: 'small' }} />
+        </IconButton>
+      )
+    });
+  };
+
+  return { showSnackbar };
+};
