315158f0dd580eefbaf77bf19ee66cc23095ed79
Julián Vuoso <juvuoso@itba.edu.ar>
2023-08-04 10:27:17 -0300

    RoundedTabs component

---
+++ lib/components/RoundedTabs.d.ts
@@ -0,0 +1,7 @@
+type Props = {
+    tabs: any[];
+    tabIndex: number;
+    onChange: (event: any, value: any) => void;
+};
+declare const RoundedTabs: ({ tabs, tabIndex, onChange }: Props) => import("react/jsx-runtime").JSX.Element;
+export default RoundedTabs;
