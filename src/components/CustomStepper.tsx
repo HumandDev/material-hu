888766660aea2c053124e5da29402e64a5765527
Julián Vuoso <juvuoso@itba.edu.ar>
2023-09-20 16:45:24 -0300

    add checked icon

--- src/components/CustomStepper.tsx
+++ src/components/CustomStepper.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { Box, Step, StepConnector, StepContent, StepIconProps, StepLabel, Stepper, StepperProps } from '@mui/material';
+import { Check as CheckIcon } from '@mui/icons-material';
 
 export type CustomStepperProps = {
   steps: {
@@ -10,16 +11,24 @@ export type CustomStepperProps = {
 };
 
 const CUSTOM_STEP_ICON_SIZE = 24;
-const CustomStepIcon = ({ active }:StepIconProps) => (
+const CustomStepIcon = ({ active, completed }:StepIconProps) => (
   <Box sx={{
     borderRadius: 3,
     width: CUSTOM_STEP_ICON_SIZE,
     height: CUSTOM_STEP_ICON_SIZE,
     border: '2px solid',
     borderColor: active ? 'primary.main' : 'text.disabled',
+    backgroundColor: completed ? 'primary.main' : 'none',
     mr: 3
   }}
-  />
+  >
+    {completed && (
+      <CheckIcon
+        sx={{ color: 'primary.contrastText' }}
+        fontSize="small"
+      />
+    )}
+  </Box>
 );
 
 const CustomStepConnector = () => (
