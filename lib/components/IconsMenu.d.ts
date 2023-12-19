0f8f8b5d77b44f320281e54fcd1b770c6cc9e167
dgarcia-hu <delfina.garcia@humand.co>
2023-12-19 13:00:14 -0300

    run tsc

--- lib/components/IconsMenu.d.ts
+++ lib/components/IconsMenu.d.ts
@@ -1,14 +1,17 @@
-import * as React from 'react';
+import { FC, MouseEvent } from 'react';
 import { SxProps } from '@mui/material';
-type Props = {
-    options: {
-        onClick: () => void;
-        label: string;
-        icon?: React.ReactNode;
-        divider?: boolean;
-        textProps?: SxProps;
-        color?: string;
-    }[];
+export type Option = {
+    onClick: () => void;
+    label: string;
+    icon?: React.ReactNode;
+    divider?: boolean;
+    textProps?: SxProps;
+    color?: string;
 };
-declare const IconsMenu: ({ options }: Props) => import("react/jsx-runtime").JSX.Element;
+export type IconsMenuProps = {
+    options: Option[];
+    onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+    onClose: (event: MouseEvent) => void;
+};
+export declare const IconsMenu: FC<IconsMenuProps>;
 export default IconsMenu;
