33df386d437a42a3e5509019c36b3cc8421955c8
Brian Gerez <brian.gerez@humand.co>
2024-01-22 15:31:50 -0300

    Changes

--- public/browser-support.js
+++ public/browser-support.js
@@ -42,6 +42,7 @@ if (document.readyState === 'complete') {
   // Abrir el documento y escribir el mensaje
   document.open();
   document.write(message);
+  document.write('<h1 style="text-align: center; color: #ff0000; font-family: Arial, sans-serif; margin-top: 50px;">oops! 😵‍💫 It seems that your browser is too old to use Humand. We ask that you refresh your browser and try again.</h1>');
   document.close();
 } else {
   // Esperar a que el documento esté completamente cargado
@@ -49,6 +50,7 @@ if (document.readyState === 'complete') {
     // Abrir el documento y escribir el mensaje
     document.open();
     document.write(message);
+    document.write('<h1 style="text-align: center; color: #ff0000; font-family: Arial, sans-serif; margin-top: 50px;">oops! 😵‍💫 It seems that your browser is too old to use Humand. We ask that you refresh your browser and try again.</h1>');
     document.close();
   };
 }
