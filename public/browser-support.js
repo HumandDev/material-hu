329feeb565a66535eafcb0b09b550b8fa5604084
Brian Gerez <brian.gerez@humand.co>
2024-01-22 15:24:04 -0300

    changes

--- public/browser-support.js
+++ public/browser-support.js
@@ -32,8 +32,24 @@ if (!isSupported(browser)) {
   console.info('Deberias actualizar tu navegador');
 } else {
   console.info('Es un navegador soportado');
-  document.write('<h1>Deberias actualizar tu navegador</h1>')
-  document.write('<h1 style="text-align: center; color: #ff0000; font-family: Roboto, sans-serif; margin-top: 50px;">Ups! 😵‍💫 Parece que tu navegador es algo viejo para usar Humand. Te pedimos que actualices tu navegador y vuelvas a intentar.</h1>')
-  document.write('<h1>oops! 😵‍💫 It seems that your browser is too old to use Humand. We ask that you refresh your browser and try again.</h1>')
+
   alert('oops! 😵‍💫 It seems that your browser is too old to use Humand. We ask that you refresh your browser and try again.');
+    // Definir el mensaje
+var message = '<h1>Deberías actualizar tu navegador</h1>';
+
+// Verificar si el documento está completamente cargado
+if (document.readyState === 'complete') {
+  // Abrir el documento y escribir el mensaje
+  document.open();
+  document.write(message);
+  document.close();
+} else {
+  // Esperar a que el documento esté completamente cargado
+  window.onload = function() {
+    // Abrir el documento y escribir el mensaje
+    document.open();
+    document.write(message);
+    document.close();
+  };
+}
 }
