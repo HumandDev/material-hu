5496e81c8429f8562cc71f867dc3f281a2d28c94
Pepi <delfina.garcia@humand.co>
2024-02-29 09:39:39 -0300

    [STORY] Accept videos on FormDropPicture (#31)
    Cambios realizados:
    - Se cambia el componente FormDropPicture a FormDrop.
    - Se recibe si se quiere que el FormDrop sea de imagen o video, teniendo
      varias props definidas por default según el tipo. Las mismas se
      permiten sobreescribir.

--- src/components/FormDrop.tsx
+++ src/components/FormDrop.tsx
@@ -16,37 +16,70 @@ import {
   alpha,
 } from '@mui/material';
 import { Upload } from '@mui/icons-material';
+import { megabytesToBytes } from '../utils/bytes';
 
-export type FormDropPictureProps = {
-  name: string;
-  rules?: any;
-  maxSize: number;
-  recommendedWidth: number;
-  recommendedHeight: number;
-  getErrorMessage?: (errorCode: ErrorCode) => string;
-  altLabel?: string;
-  deleteLabel?: string;
-  helpTextLabel?: string;
-  linkLabel?: string;
-  label?: string;
+export enum FormDropTypes {
+  IMAGE = 'image',
+  VIDEO = 'video',
 };
 
-export const FormDropPicture: FC<FormDropPictureProps> = (props) => {
+const ACCEPT_BY_TYPE = {
+  [FormDropTypes.IMAGE]: { 'image/png': [], 'image/jpeg': [] },
+  [FormDropTypes.VIDEO]: { 'video/mp4': [] },
+};
+
+const MAX_SIZE_BY_TYPE = {
+  [FormDropTypes.IMAGE]: megabytesToBytes(100),
+  [FormDropTypes.VIDEO]: megabytesToBytes(150),
+};
+
+const RECOMMENDED_WIDTH = 900;
+const RECOMMENDED_HEIGHT = 400;
+
+export type FormDropContext = {
+  maxSize: number;
+  recommendedHeight: number;
+  recommendedWidth: number;
+  type: FormDropTypes;
+};
+
+export type FormDropProps = {
+  name: string;
+  rules?: any;
+  maxSize?: number;
+  recommendedWidth?: number;
+  recommendedHeight?: number;
+  getErrorMessage?: (errorCode: ErrorCode, context: FormDropContext) => string;
+  altLabel?: (context: FormDropContext) => string;
+  deleteLabel?: (context: FormDropContext) => string;
+  helpTextLabel?: (context: FormDropContext) => string;
+  linkLabel?: (context: FormDropContext) => string;
+  label?: (context: FormDropContext) => string;
+  accept?: Record<string, string[]>;
+  type?: FormDropTypes;
+};
+
+export const FormDrop: FC<FormDropProps> = (props) => {
   const {
     name,
     rules,
-    maxSize,
-    recommendedWidth,
-    recommendedHeight,
+    maxSize: maxSizeProp,
+    recommendedWidth = RECOMMENDED_WIDTH,
+    recommendedHeight = RECOMMENDED_HEIGHT,
     getErrorMessage = () => '',
-    altLabel = '',
-    deleteLabel = '',
-    helpTextLabel = '',
-    linkLabel = '',
-    label = '',
+    altLabel = () => '',
+    deleteLabel = () => '',
+    helpTextLabel = () => '',
+    linkLabel = () => '',
+    label = () => '',
+    accept: acceptProp,
+    type = FormDropTypes.IMAGE,
   } = props;
 
   const theme = useTheme();
+
+  const accept = acceptProp || ACCEPT_BY_TYPE[type];
+  const maxSize = maxSizeProp || MAX_SIZE_BY_TYPE[type];
 
   const {
     control,
@@ -63,6 +96,13 @@ export const FormDropPicture: FC<FormDropPictureProps> = (props) => {
       control={control}
       rules={rules}
       render={({ field: { onChange, value } }) => {
+        const context = {
+          maxSize,
+          recommendedHeight,
+          recommendedWidth,
+          type,
+        };
+
         const handleDrop = (files: File[]) => {
           if (!files[0]) return;
 
@@ -83,30 +123,36 @@ export const FormDropPicture: FC<FormDropPictureProps> = (props) => {
 
           setError(name, {
             type: 'custom',
-            message: getErrorMessage(fileRejections[0].errors[0].code as ErrorCode),
+            message: getErrorMessage(
+              fileRejections[0].errors[0].code as ErrorCode,
+              context,
+            ),
           });
         };
 
         const { getRootProps, getInputProps, isDragActive } = useDropzone({
           onDrop: handleDrop,
           onDropRejected: handleError,
-          accept: { 'image/png': [], 'image/jpeg': [] },
+          accept,
           maxFiles: 1,
           multiple: false,
           maxSize,
         });
+
+        const hasValue = value?.url?.length > 0 || !!value?.file;
 
         return (
           <Stack
             spacing={3}
             width="100%"
           >
-            {value && (
+            {hasValue && (
               <>
                 <Box
-                  component="img"
+                  component={type === FormDropTypes.IMAGE ? "img" : "video"}
                   src={value.url || URL.createObjectURL(value.file)}
-                  alt={altLabel}
+                  alt={altLabel(context)}
+                  controls
                   sx={{
                     width: '100%',
                     height: 'auto',
@@ -120,11 +166,11 @@ export const FormDropPicture: FC<FormDropPictureProps> = (props) => {
                   onClick={handleDelete}
                   sx={{ width: 'fit-content' }}
                 >
-                  {deleteLabel}
+                  {deleteLabel(context)}
                 </Button>
               </>
             )}
-            {!value && (
+            {!hasValue && (
               <>
                 <Box
                   aria-describedby="drop-picture-error-text"
@@ -175,16 +221,16 @@ export const FormDropPicture: FC<FormDropPictureProps> = (props) => {
                       component="span"
                     >
                       <Link color="primary">
-                        {linkLabel}
+                        {linkLabel(context)}
                       </Link>
-                      {label}
+                      {label(context)}
                     </Typography>
                     <Typography
                       variant="body2"
                       component="span"
                       color="textSecondary"
                     >
-                      {helpTextLabel}
+                      {helpTextLabel(context)}
                     </Typography>
                   </Stack>
                 </Box>
@@ -204,4 +250,4 @@ export const FormDropPicture: FC<FormDropPictureProps> = (props) => {
     />
   );
 };
-export default FormDropPicture;
+export default FormDrop;
