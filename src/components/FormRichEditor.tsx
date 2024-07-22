036e3d34ce19e508f7c5c7a476a1dcac004f63f7
Mariano Zicavo <marianozicavo@gmail.com>
2024-07-22 15:26:21 -0300

    wip

--- src/components/FormRichEditor.tsx
+++ src/components/FormRichEditor.tsx
@@ -5,53 +5,54 @@ import { Editor as EditorType } from 'tinymce';
 
 // https://www.tiny.cloud/docs/tinymce/latest/react-zip-bundle/
 // TinyMCE so the global var exists
-import '../../vendors/tinymce/tinymce.min.js';
+import 'tinymce/tinymce';
 // DOM model
-import '../../vendors/tinymce/models/dom/model.min.js';
+import 'tinymce/models/dom/model';
 // Theme
-import '../../vendors/tinymce/themes/silver/theme.min.js';
+import 'tinymce/themes/silver';
 // Toolbar icons
-import '../../vendors/tinymce/icons/default/icons.min.js';
+import 'tinymce/icons/default';
 // Editor styles
-import '../../vendors/tinymce/skins/ui/oxide/skin.js';
+import 'tinymce/skins/ui/oxide/skin';
 
-// importing the plugin index.js file.
+// importing the plugin js.
 // if you use a plugin that is not listed here the editor will fail to load
-import '../../vendors/tinymce/plugins/advlist/plugin.min.js';
-import '../../vendors/tinymce/plugins/anchor/plugin.min.js';
-import '../../vendors/tinymce/plugins/autolink/plugin.min.js';
-import '../../vendors/tinymce/plugins/autoresize/plugin.min.js';
-import '../../vendors/tinymce/plugins/autosave/plugin.min.js';
-import '../../vendors/tinymce/plugins/charmap/plugin.min.js';
-import '../../vendors/tinymce/plugins/code/plugin.min.js';
-import '../../vendors/tinymce/plugins/codesample/plugin.min.js';
-import '../../vendors/tinymce/plugins/directionality/plugin.min.js';
-import '../../vendors/tinymce/plugins/emoticons/plugin.min.js';
-import '../../vendors/tinymce/plugins/fullscreen/plugin.min.js';
-import '../../vendors/tinymce/plugins/help/plugin.min.js';
-import '../../vendors/tinymce/plugins/image/plugin.min.js';
-import '../../vendors/tinymce/plugins/importcss/plugin.min.js';
-import '../../vendors/tinymce/plugins/insertdatetime/plugin.min.js';
-import '../../vendors/tinymce/plugins/link/plugin.min.js';
-import '../../vendors/tinymce/plugins/lists/plugin.min.js';
-import '../../vendors/tinymce/plugins/media/plugin.min.js';
-import '../../vendors/tinymce/plugins/nonbreaking/plugin.min.js';
-import '../../vendors/tinymce/plugins/pagebreak/plugin.min.js';
-import '../../vendors/tinymce/plugins/preview/plugin.min.js';
-import '../../vendors/tinymce/plugins/quickbars/plugin.min.js';
-import '../../vendors/tinymce/plugins/save/plugin.min.js';
-import '../../vendors/tinymce/plugins/searchreplace/plugin.min.js';
-import '../../vendors/tinymce/plugins/table/plugin.min.js';
-import '../../vendors/tinymce/plugins/visualblocks/plugin.min.js';
-import '../../vendors/tinymce/plugins/visualchars/plugin.min.js';
-import '../../vendors/tinymce/plugins/wordcount/plugin.min.js';
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/anchor';
+import 'tinymce/plugins/autolink';
+import 'tinymce/plugins/autoresize';
+import 'tinymce/plugins/autosave';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/directionality';
+import 'tinymce/plugins/emoticons';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/help';
+import 'tinymce/plugins/help/js/i18n/keynav/en';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/importcss';
+import 'tinymce/plugins/insertdatetime';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/nonbreaking';
+import 'tinymce/plugins/pagebreak';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/quickbars';
+import 'tinymce/plugins/save';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/visualblocks';
+import 'tinymce/plugins/visualchars';
+import 'tinymce/plugins/wordcount';
 
 // importing plugin resources
-import '../../vendors/tinymce/plugins/emoticons/js/emojis.js';
+import 'tinymce/plugins/emoticons/js/emojis';
 
 // Content styles, including inline UI like fake cursors
-import '../../vendors/tinymce/skins/content/default/content.js';
-import '../../vendors/tinymce/skins/ui/oxide/content.js';
+import 'tinymce/skins/content/default/content';
+import 'tinymce/skins/ui/oxide/content';
 
 type Props = UseControllerProps & {
   editorProps?: IAllProps;
