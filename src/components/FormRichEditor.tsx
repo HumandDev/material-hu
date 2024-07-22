32ef794c5432e05d3da4c0d73bffd2049ce8bddd
Mariano Zicavo <marianozicavo@gmail.com>
2024-07-22 13:34:48 -0300

    wip

--- src/components/FormRichEditor.tsx
+++ src/components/FormRichEditor.tsx
@@ -118,6 +118,7 @@ function FormRichEditor({
           apiKey={tinyKey}
           onBlur={() => (handleBlur ? handleBlur(field.value) : null)}
           disabled={disabled}
+          tinymceScriptSrc="/tinymce/tinymce.min.js"
         />
       )}
       {...props}
