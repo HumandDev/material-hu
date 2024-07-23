000088655f35508a50d262f3879f5f27fc2eb0f1
Mariano Zicavo <marianozicavo@users.noreply.github.com>
2024-07-23 16:23:59 -0300

    [SQJG-2791] TinyMCE selfhosted (#58)
    * bundle tinymce
    
    * Ignore DS_STORE

--- src/components/FormRichEditor.tsx
+++ src/components/FormRichEditor.tsx
@@ -3,6 +3,59 @@ import i18next from 'i18next';
 import { Editor, IAllProps } from '@tinymce/tinymce-react';
 import { Editor as EditorType } from 'tinymce';
 
+// https://www.tiny.cloud/docs/tinymce/latest/react-zip-bundle/
+// TinyMCE so the global var exists
+import 'tinymce/tinymce';
+// DOM model
+import 'tinymce/models/dom/model';
+// Theme
+import 'tinymce/themes/silver';
+// Toolbar icons
+import 'tinymce/icons/default';
+// Editor styles
+import 'tinymce/skins/ui/oxide/skin';
+
+// importing the plugin js.
+// if you use a plugin that is not listed here the editor will fail to load
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/anchor';
+import 'tinymce/plugins/autolink';
+import 'tinymce/plugins/autoresize';
+import 'tinymce/plugins/autosave';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/directionality';
+import 'tinymce/plugins/emoticons';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/help';
+import 'tinymce/plugins/help/js/i18n/keynav/en';
+import 'tinymce/plugins/help/js/i18n/keynav/es';
+import 'tinymce/plugins/help/js/i18n/keynav/pt_BR';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/importcss';
+import 'tinymce/plugins/insertdatetime';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/nonbreaking';
+import 'tinymce/plugins/pagebreak';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/quickbars';
+import 'tinymce/plugins/save';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/visualblocks';
+import 'tinymce/plugins/visualchars';
+import 'tinymce/plugins/wordcount';
+
+// importing plugin resources
+import 'tinymce/plugins/emoticons/js/emojis';
+
+// Content styles, including inline UI like fake cursors
+import 'tinymce/skins/content/default/content';
+import 'tinymce/skins/ui/oxide/content';
+
 type Props = UseControllerProps & {
   editorProps?: IAllProps;
   onEditorChange?: (value: string, editor: EditorType) => string;
@@ -14,7 +67,6 @@ type Props = UseControllerProps & {
   placeholder?: string;
   imagesUploadHandler: any;
   addVideoEditorPlugin: Function;
-  tinyKey: string;
   fontsURL: string;
 };
 
@@ -51,7 +103,6 @@ function FormRichEditor({
   disabled = false,
   imagesUploadHandler,
   addVideoEditorPlugin,
-  tinyKey,
   fontsURL,
   ...props
 }: Props) {
@@ -115,7 +166,7 @@ function FormRichEditor({
           onEditorChange={handleEditorChange(onChange)}
           onInit={onInit}
           {...allEditorProps}
-          apiKey={tinyKey}
+          apiKey={'gpl'}
           onBlur={() => (handleBlur ? handleBlur(field.value) : null)}
           disabled={disabled}
         />
