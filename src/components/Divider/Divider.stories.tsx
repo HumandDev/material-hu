5d55e408ab35aa495065fe342c2ba4c912088d8f
Sebastian Favaron <sebastian.favaron@humand.co>
2024-08-22 14:17:34 -0300

    Refactor Divider story
    + limit props
    + simplify example

--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -1,21 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Divider, List, ListItem, ListItemText, Chip } from '@mui/material';
+import { Box, Divider } from '@mui/material';
 
 const meta: Meta<typeof Divider> = {
   component: Divider,
   title: 'Divider',
   tags: ['autodocs'],
   argTypes: {
-    absolute: {
-      table: {
-        defaultValue: { summary: 'false' },
-      },
-    },
-    flexItem: {
-      table: {
-        defaultValue: { summary: 'false' },
-      },
-    },
     orientation: {
       control: 'radio',
       options: ['horizontal', 'vertical'],
@@ -23,55 +13,28 @@ const meta: Meta<typeof Divider> = {
         defaultValue: { summary: 'horizontal' },
       },
     },
-    textAlign: {
-      control: 'radio',
-      options: ['center', 'left', 'rigth'],
-      table: {
-        defaultValue: { summary: 'center' },
-      },
-    },
-    variant: {
-      control: 'radio',
-      options: ['fullWidth', 'inset', 'middle'],
-      table: {
-        defaultValue: { summary: 'fullWidth' },
-      },
-    },
-    children: {
-      control: 'text',
-      table: {
-        type: { summary: 'ReactNode' },
-      },
-    },
   },
   args: {
-    absolute: false,
-    flexItem: false,
     orientation: 'horizontal',
-    textAlign: 'center',
-    variant: 'fullWidth',
   },
   decorators: [
-    Story => (
-      <List
+    (Story, context) => (
+      <Box
         sx={{
-          py: 0,
-          width: '100%',
-          maxWidth: 360,
-          borderRadius: 2,
-          border: '1px solid',
-          borderColor: 'divider',
-          backgroundColor: 'background.paper',
+          p: 2,
+          display: 'flex',
+          flexDirection:
+            context.args.orientation === 'vertical' ? 'row' : 'column',
+          maxWidth: context.args.orientation === 'vertical' ? 'auto' : 50,
+          '& hr': {
+            height: 'auto',
+          },
         }}
       >
-        <ListItem>
-          <ListItemText primary="Lorem ipsum odor amet" />
-        </ListItem>
+        <Box p={0.5}>Item 1</Box>
         <Story />
-        <ListItem>
-          <ListItemText primary="Lorem ipsum odor amet" />
-        </ListItem>
-      </List>
+        <Box p={0.5}>Item 2</Box>
+      </Box>
     ),
   ],
 };
@@ -84,25 +47,8 @@ export const Default: Story = {
   args: {},
 };
 
-export const Inset: Story = {
+export const Vertical: Story = {
   args: {
-    variant: 'inset',
-  },
-};
-
-export const Middle: Story = {
-  args: {
-    variant: 'middle',
-  },
-};
-
-export const WithChildren: Story = {
-  args: {
-    children: (
-      <Chip
-        label="chip"
-        size="small"
-      />
-    ),
+    orientation: 'vertical',
   },
 };
