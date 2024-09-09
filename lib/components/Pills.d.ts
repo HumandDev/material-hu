7a2f29d8f60450281c49f7fd6c3d535357036b7e
Julián Vuoso <juvuoso@itba.edu.ar>
2024-09-09 12:19:24 -0300

    rerun tsc

--- lib/components/Pills.d.ts
+++
@@ -1,9 +0,0 @@
-import { ChipProps } from '@mui/material';
-export type PillsProps = ChipProps & {
-    label: string;
-    type?: 'default' | 'error' | 'success' | 'warning' | 'info' | 'highlight' | 'neutral' | 'disabled';
-    size?: 'small' | 'medium';
-    hasIcon?: boolean;
-};
-declare const Pills: ({ label, type, size, hasIcon, sx, }: PillsProps) => import("react/jsx-runtime").JSX.Element;
-export default Pills;
