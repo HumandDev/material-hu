9504a4e606a56f2d36a528b8ace36a50f07c9e9a
Mariano Zicavo <mariano.zicavo@humand.co>
2024-09-24 17:21:41 -0300

    Add index to tab change

--- lib/components/Tabs/Tabs.d.ts
+++ lib/components/Tabs/Tabs.d.ts
@@ -3,7 +3,7 @@ type Props = Pick<TabsProps, 'sx' | 'defaultValue'> & {
     tabs: {
         label: string;
     }[];
-    onTabChange?: (value: string) => void;
+    onTabChange?: (value: string, index: number) => void;
 };
 declare const Tabs: ({ tabs, sx, onTabChange, defaultValue }: Props) => import("react/jsx-runtime").JSX.Element;
 export default Tabs;
