a0fb5560356c2fdb0c159d68e9000bca27afe2c5
Julián Vuoso <juvuoso@itba.edu.ar>
2024-10-17 10:46:29 -0300

    try new provider

--- lib/components/SnackbarHugo/SnackbarShow.d.ts
+++ lib/components/SnackbarHugo/SnackbarShow.d.ts
@@ -1,7 +1,7 @@
-/// <reference types="react" />
+import React from 'react';
 import { SnackbarProps } from './SnackbarWrapper';
 type SnackbarContextType = {
-    showSnackbar: (props: SnackbarProps) => void;
+    enqueueSnackbar: (props: SnackbarProps) => void;
 };
 export declare const useSnackbar: () => SnackbarContextType;
 export declare const SnackbarProvider: React.FC<{
--- src/components/SnackbarHugo/SnackbarShow.tsx
+++ src/components/SnackbarHugo/SnackbarShow.tsx
@@ -1,8 +1,8 @@
-import { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import SnackbarWrapper, { SnackbarProps } from './SnackbarWrapper';
 
 type SnackbarContextType = {
-  showSnackbar: (props: SnackbarProps) => void;
+  enqueueSnackbar: (props: SnackbarProps) => void;
 };
 
 const SnackbarContext = createContext<SnackbarContextType | undefined>(
@@ -11,22 +11,37 @@ const SnackbarContext = createContext<SnackbarContextType | undefined>(
 
 export const useSnackbar = () => {
   const context = useContext(SnackbarContext);
-  return context || { showSnackbar: () => {} };
+  return context || { enqueueSnackbar: () => {} };
 };
 
 export const SnackbarProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [snackbarQueue, setSnackbarQueue] = useState<SnackbarProps[]>([]);
+  const [visibleSnackbars, setVisibleSnackbars] = useState<SnackbarProps[]>([]);
 
-  const showSnackbar = (props: SnackbarProps) => {
-    setSnackbarQueue(prev => [...prev, props]); // Agregar nuevo Snackbar a la cola
+  const enqueueSnackbar = (props: SnackbarProps) => {
+    setSnackbarQueue(prevQueue => [...prevQueue, props]);
   };
 
+  useEffect(() => {
+    // Si hay menos de 3 snackbars visibles, mostrar el siguiente en cola
+    if (visibleSnackbars.length < 3 && snackbarQueue.length > 0) {
+      setVisibleSnackbars(prev => [...prev, snackbarQueue[0]]);
+      setSnackbarQueue(prevQueue => prevQueue.slice(1));
+    }
+
+    // Si hay más de 3 snackbars, cerrar el más antiguo (el primero de la lista)
+    if (visibleSnackbars.length === 3 && snackbarQueue.length > 0) {
+      setVisibleSnackbars(prev => [...prev.slice(1), snackbarQueue[0]]);
+      setSnackbarQueue(prevQueue => prevQueue.slice(1));
+    }
+  }, [snackbarQueue, visibleSnackbars]);
+
   return (
-    <SnackbarContext.Provider value={{ showSnackbar }}>
+    <SnackbarContext.Provider value={{ enqueueSnackbar }}>
       {children}
-      {snackbarQueue.map((snackbarProps, index) => (
+      {visibleSnackbars.map((snackbarProps, index) => (
         <SnackbarWrapper
           key={index}
           {...snackbarProps}
