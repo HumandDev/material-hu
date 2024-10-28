751fe735683f6a4ce3a54ecde4e98114ff19c685
Julián Vuoso <juvuoso@itba.edu.ar>
2024-10-28 15:10:34 -0300

    update exports

--- src/huexports.ts
+++ src/huexports.ts
@@ -13,7 +13,4 @@ export { default as HuTabs } from './components/Tabs/Tabs';
 export { default as HuInputClassic } from './components/Input/InputClassic';
 export { default as HuFormInputClassic } from './components/Input/FormInputClassic';
 export { default as HuFormSelectionCard } from './components/SelectionCard/FormSelectionCard';
-export {
-  useMySnackbar,
-  MySnackbarProvider,
-} from './components/SnackbarHugo/Snackbar2';
+export { default as useHuSnackbar } from './components/SnackbarHugo/Snackbar2';
