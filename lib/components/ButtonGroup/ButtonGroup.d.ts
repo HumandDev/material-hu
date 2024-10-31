f0945c7a5e9e8a36ec93add0f5a6dbec66bc84cd
Julián Vuoso <juvuoso@itba.edu.ar>
2024-10-31 17:11:19 -0300

    new prop buttonGroup

--- lib/components/ButtonGroup/ButtonGroup.d.ts
+++ lib/components/ButtonGroup/ButtonGroup.d.ts
@@ -1,6 +1,7 @@
 export type ButtonGroupProps = {
     labels: [string, string] | [string, string, string];
     onChange?: (index: number) => void;
+    fullWidth?: boolean;
 };
-declare const ButtonGroup: ({ labels, onChange }: ButtonGroupProps) => import("react/jsx-runtime").JSX.Element;
+declare const ButtonGroup: ({ labels, onChange, fullWidth, }: ButtonGroupProps) => import("react/jsx-runtime").JSX.Element;
 export default ButtonGroup;
