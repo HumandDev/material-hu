c183ab8c60809ecb170b5b5a95c1eeea1bffe87e
Lucas Zibell <lucaszibell@Lucass-MacBook-Pro.local>
2024-11-08 17:39:05 -0300

    run tsc

--- lib/components/Drawer/Drawer.d.ts
+++ lib/components/Drawer/Drawer.d.ts
@@ -1,17 +1,14 @@
 import { ReactNode } from 'react';
-import { DrawerProps as MuiDrawerProps } from '@mui/material';
-type DrawerProps = MuiDrawerProps & {
+import { DrawerProps as MuiDrawerProps, ButtonProps } from '@mui/material';
+export type DrawerProps = MuiDrawerProps & {
     title?: string;
     size?: 'medium' | 'large';
     onClose: () => void;
-    primaryActionText?: string;
-    secondaryActionText?: string;
-    primaryAction?: () => void;
-    secondaryAction?: () => void;
-    extraFooterContent?: ReactNode;
-    withDoubleLayout?: boolean;
-    primaryLayoutContent?: ReactNode;
-    secondaryLayoutContent?: ReactNode;
+    primaryButtonProps?: ButtonProps;
+    secondaryButtonProps?: ButtonProps;
+    footer?: ReactNode;
+    primaryContent?: ReactNode;
+    secondaryContent?: ReactNode;
 };
 declare const Drawer: (props: DrawerProps) => import("react/jsx-runtime").JSX.Element;
 export default Drawer;
