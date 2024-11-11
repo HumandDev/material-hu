b91000793572283fd50b1396a811e80d43639097
Julián Vuoso <95429033+jvuoso-hu@users.noreply.github.com>
2024-11-11 17:46:54 -0300

    [-] Improvements Snackbar & ButtonGroup (#126)
    * some improvements
    
    * onchange handling
    
    ---------

--- lib/components/ButtonGroup/ButtonGroup.d.ts
+++ lib/components/ButtonGroup/ButtonGroup.d.ts
@@ -2,6 +2,7 @@ export type ButtonGroupProps = {
     labels: [string, string] | [string, string, string];
     onChange?: (index: number) => void;
     fullWidth?: boolean;
+    disableUnselect?: boolean;
 };
-declare const ButtonGroup: ({ labels, onChange, fullWidth, }: ButtonGroupProps) => import("react/jsx-runtime").JSX.Element;
+declare const ButtonGroup: ({ labels, onChange, fullWidth, disableUnselect, }: ButtonGroupProps) => import("react/jsx-runtime").JSX.Element;
 export default ButtonGroup;
--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -6,18 +6,29 @@ export type ButtonGroupProps = {
   labels: [string, string] | [string, string, string]; // Min 3 buttons - Max 3 buttons
   onChange?: (index: number) => void;
   fullWidth?: boolean;
+  disableUnselect?: boolean;
 };
 
 const ButtonGroup = ({
   labels,
   onChange,
   fullWidth = false,
+  disableUnselect = false,
 }: ButtonGroupProps) => {
   const [selectedButton, setSelectedButton] = useState<number | null>(0);
 
   const buttonSelection = (index: number) => {
-    setSelectedButton(prevSelected => (prevSelected === index ? null : index));
-    onChange?.(index);
+    const newIndex = selectedButton === index ? null : index;
+    const newSelected = disableUnselect ? index : newIndex;
+    setSelectedButton(newSelected);
+
+    // Execute onChange if its enabled Unselect or if its a different button
+    if (
+      !disableUnselect ||
+      (disableUnselect && newSelected !== selectedButton)
+    ) {
+      onChange?.(index);
+    }
   };
 
   const {
