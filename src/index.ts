3dba592843b31dc24aa668d63ccaf4b8374b3f7f
Brian Gerez <brian.gerez@humand.co>
2024-11-28 14:54:06 -0300

    test button

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,11 @@ export * from '@mui/material';
 export * from '@mui/x-date-pickers';
 export * from '@mui/x-date-pickers/AdapterDateFns';
 export * from '@mui/x-date-pickers/internals/hooks/useDesktopPicker';
-export {
-  default as Button,
-  type LoadingButtonProps as ButtonProps,
-} from '@mui/lab/LoadingButton';
+// export {
+//   default as Button,
+//   type LoadingButtonProps as ButtonProps,
+// } from '@mui/lab/LoadingButton';
+export { Button, ButtonProps } from '@mui/material';
 export { TreeView, TreeItem } from '@mui/lab';
 export { createNewTheme } from './theme/material-hu';
 export { default as IconsMenu } from './components/IconsMenu';
