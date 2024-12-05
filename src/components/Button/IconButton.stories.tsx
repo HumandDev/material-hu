2196a5e2653074e23f477692575a59dbe190381e
bgerez-hu <144142293+bgerez-hu@users.noreply.github.com>
2024-12-05 13:50:34 -0300

    [SQSH-1848] DS3 | Buttons 2 (#146)
    * se agregan las variantes primary, secondary y tertiary & se agregan los sizes small y large para los Button e IconButtons. Se mantiene retrocompatibilidad con las variantes anteriores y con el size medium. Para los Fab se agrego el size large. Se adapta Dropdown con las nuevas variantes de Button. Tambien se agregaron los storybook deIconButton y Fab y se modifico el de Button
    
    * upgrade material ui to 5.16.8 for web & admin projects support the new variants styles and new variants props
    
    * stories de Dialog, Drawer & Menu modificados con las nuevas variantes
    
    * remove capitalize of button styles

---
+++ src/components/Button/IconButton.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { IconInfoCircle } from '@tabler/icons-react';
+import { IconButton } from '@mui/material';
+
+const meta: Meta<typeof IconButton> = {
+  title: 'IconButton',
+  component: IconButton,
+  tags: ['autodocs'],
+  args: {
+    children: <IconInfoCircle />,
+    disabled: false,
+  },
+  argTypes: {
+    variant: {
+      control: 'radio',
+      options: ['primary', 'secondary', 'tertiary'],
+      table: {
+        defaultValue: { summary: 'tertiary' },
+      },
+    },
+    size: {
+      control: 'radio',
+      options: ['small', 'large'],
+      table: {
+        defaultValue: { summary: 'large' },
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
