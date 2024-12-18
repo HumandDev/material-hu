e42ed1ca9fee326a32a97ea37edf8e26484ad0fa
“guilleHumand” <guillermo.sequeira@humand.co>
2024-12-18 13:56:30 -0300

    fix build

--- lib/components/Input/InputClassic.d.ts
+++ lib/components/Input/InputClassic.d.ts
@@ -1,35 +1,14 @@
 import { ReactNode } from 'react';
 import { FormControlProps, SxProps } from '@mui/material';
 import { CustomInputProps } from './CustomInput';
-export type InputProps = Pick<
-  FormControlProps,
-  'sx' | 'fullWidth' | 'disabled'
-> & {
-  label?: string;
-  helperText?: string;
-  errorText?: string;
-  error?: boolean;
-  hasCounter?: boolean;
-  startAdornment?: ReactNode;
-  sxInput?: SxProps;
+export type InputProps = Pick<FormControlProps, 'sx' | 'fullWidth' | 'disabled'> & {
+    label?: string;
+    helperText?: string;
+    errorText?: string;
+    error?: boolean;
+    hasCounter?: boolean;
+    startAdornment?: ReactNode;
+    sxInput?: SxProps;
 } & CustomInputProps;
-declare const InputClassic: ({
-  sx,
-  label,
-  value,
-  helperText,
-  errorText,
-  onChange,
-  placeholder,
-  inputRef,
-  error,
-  success,
-  maxLength,
-  hasCounter,
-  fullWidth,
-  multiline,
-  startAdornment,
-  sxInput,
-  disabled,
-}: InputProps) => import('react/jsx-runtime').JSX.Element;
+declare const InputClassic: ({ sx, label, value, helperText, errorText, onChange, placeholder, inputRef, error, success, maxLength, hasCounter, fullWidth, multiline, startAdornment, sxInput, disabled, }: InputProps) => import("react/jsx-runtime").JSX.Element;
 export default InputClassic;
