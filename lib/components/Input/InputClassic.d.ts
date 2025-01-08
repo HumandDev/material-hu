9d85b64c51d70e91d863fd9e1080e3f5e5f6ba89
gsequeira-hu <93542548+gsequeira-hu@users.noreply.github.com>
2025-01-08 16:12:39 -0300

    delete sxInput (#166)
    Co-authored-by: “guilleHumand” <“guillermo.sequeira@humand.co”>

--- lib/components/Input/InputClassic.d.ts
+++ lib/components/Input/InputClassic.d.ts
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { FormControlProps, SxProps } from '@mui/material';
+import { FormControlProps } from '@mui/material';
 import { CustomInputProps } from './CustomInput';
 export type InputProps = Pick<FormControlProps, 'sx' | 'fullWidth' | 'disabled'> & {
     label?: string;
@@ -8,7 +8,6 @@ export type InputProps = Pick<FormControlProps, 'sx' | 'fullWidth' | 'disabled'>
     error?: boolean;
     hasCounter?: boolean;
     startAdornment?: ReactNode;
-    sxInput?: SxProps;
 } & CustomInputProps;
-declare const InputClassic: ({ sx, label, value, helperText, errorText, onChange, placeholder, inputRef, error, success, maxLength, hasCounter, fullWidth, multiline, startAdornment, sxInput, disabled, }: InputProps) => import("react/jsx-runtime").JSX.Element;
+declare const InputClassic: ({ sx, label, value, helperText, errorText, onChange, placeholder, inputRef, error, success, maxLength, hasCounter, fullWidth, multiline, startAdornment, disabled, }: InputProps) => import("react/jsx-runtime").JSX.Element;
 export default InputClassic;
