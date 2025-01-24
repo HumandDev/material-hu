28d605b54e09686bb4441ee024c1f044079b1d81
dgarcia-hu <delfina.garcia@humand.co>
2025-01-24 10:23:49 -0300

    fix useHuPagination controller props

--- lib/hooks/useHuPagination.d.ts
+++ lib/hooks/useHuPagination.d.ts
@@ -1,8 +1,12 @@
 import { FC } from 'react';
 import { FormSearchProps } from '../components/Input/FormSearch';
 import { FormPaginationProps } from '../components/Input/FormPagination';
-export type SearchControllerProps = Omit<FormSearchProps, 'name' | 'onChange'>;
-export type PaginationControllerProps = Omit<FormPaginationProps, 'name' | 'onChangeLimit' | 'onChangePage' | 'limitOptions'>;
+export type SearchControllerProps = Omit<FormSearchProps, 'name'> & {
+    inputProps?: Omit<FormSearchProps['inputProps'], 'onChange'>;
+};
+export type PaginationControllerProps = Omit<FormPaginationProps, 'name'> & {
+    inputProps?: Omit<FormPaginationProps, 'onChangeLimit' | 'onChangePage' | 'limitOptions'>;
+};
 export type PaginationParams = {
     search: string;
     pagination: {
