9022ab424808bcef2e98daa22f67c7cefc90d1bd
dgarcia-hu <delfina.garcia@humand.co>
2025-01-28 10:02:38 -0300

    lint fixes

--- src/hooks/useHuPagination.tsx
+++ src/hooks/useHuPagination.tsx
@@ -10,11 +10,17 @@ import { formatSearchParams } from '../utils/pagination';
 const SEARCH_MIN_LENGTH = 3;
 const FIRST_PAGE = 1;
 
-export type SearchControllerProps = Omit<FormSearchProps, 'name' | 'inputProps'> & {
+export type SearchControllerProps = Omit<
+  FormSearchProps,
+  'name' | 'inputProps'
+> & {
   inputProps?: Omit<FormSearchProps['inputProps'], 'onChange'>;
 };
 
-export type PaginationControllerProps = Omit<FormPaginationProps, 'name' | 'inputProps'> & {
+export type PaginationControllerProps = Omit<
+  FormPaginationProps,
+  'name' | 'inputProps'
+> & {
   inputProps?: Omit<
     FormPaginationProps['inputProps'],
     'onChangeLimit' | 'onChangePage' | 'limitOptions'
